b789bcea6429506249c2726c491ea941709cf4dd
Francisco Lemos <56840967+FranciscoLemos03@users.noreply.github.com>
2024-10-07 14:59:18 +0100

    Calculator Shape

--- calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.IntegerRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,29 +23,110 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    CalculateShape(7.10)
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CalculateShape(number: Double, modifier: Modifier = Modifier) {
+
+    Column {
+        Text(
+            text = "$number",
+            modifier = modifier
+        )
+        Row {
+            ElevatedButton(onClick = { }) {
+                Text("MRC")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("M-")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("M+")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("ON/C")
+            }
+        }
+        Row {
+            ElevatedButton(onClick = { }) {
+                Text("√")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("%")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("+/-")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("CE")
+            }
+        }
+        Row {
+            ElevatedButton(onClick = { }) {
+                Text("7")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("8")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("9")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("÷")
+            }
+        }
+        Row {
+            ElevatedButton(onClick = { }) {
+                Text("4")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("5")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("6")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("x")
+            }
+        }
+        Row {
+            ElevatedButton(onClick = { }) {
+                Text("1")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("2")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("3")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("-")
+            }
+        }
+        Row {
+            ElevatedButton(onClick = { }) {
+                Text("0")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("•")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("=")
+            }
+            ElevatedButton(onClick = { }) {
+                Text("+")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CalculatorTheme {
-        Greeting("Android")
-    }
+    CalculateShape(7.10)
 }
